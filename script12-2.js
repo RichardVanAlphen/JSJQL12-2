63a5119667291544827ea2e39dd3307674c1fcdd
RichardVanAlphen <111999466+RichardVanAlphen@users.noreply.github.com>
2024-09-30 14:51:53 +0200

    Update script12-2.js

--- script12-2.js
+++ script12-2.js
@@ -2,14 +2,11 @@ $(document).ready(function(){
     $('#btnLoad').on('click',function(){
         $.ajax({
             url: 'content12-2.html',
-            success: function(data) { // Veranderd 'succes' naar 'success'
-                console.log('Success:', data);
+            success: function(data) {
+                $('divResult').html(data);
             },
             error: function() {
-                console.log('Fout: Er is iets mis gegaan!');
-            },
-            complete: function() {
-                console.log('Complete');
+                alert('Fout: Er is iets mis gegaan!');
             }
         });
     });
