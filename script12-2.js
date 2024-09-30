8022e1a3b77c96fbcae63fe6a2e14ae15ffba540
RichardVanAlphen <111999466+RichardVanAlphen@users.noreply.github.com>
2024-09-30 15:13:26 +0200

    Update script12-2.js

--- script12-2.js
+++ script12-2.js
@@ -3,8 +3,7 @@ $(document).ready(function(){
         $.ajax({
             url: 'content12-2.html',
             success: function(data) {
-                $('divResult').html(data);
-                console.log('success');
+                $('#divResult').html(data);
             },
             error: function() {
                 alert('Fout: Er is iets mis gegaan!');
